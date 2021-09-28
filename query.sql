7eef91e1ff8e5d5ab31da57e6a7df205a22b647b
Forklein <giuseppepisani2@gmail.com>
2021-09-28 15:17:51 +0200

    Edit name

--- query.sql
+++
@@ -1,9 +0,0 @@
---# 1. Selezionare tutti gli studenti nati nel 1990 (160)
-SELECT * 
-from `students` 
-where YEAR(`date_of_birth`) = 1990;
-
---# 2. Selezionare tutti i corsi che valgono più di 10 crediti
-SELECT *
-from `courses`
-where `cfu` > 10;
