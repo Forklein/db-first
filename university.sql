55498ae8dbe003879887154f153287d10c538a70
Forklein <giuseppepisani2@gmail.com>
2021-09-28 15:47:04 +0200

    Done other tracks

--- university.sql
+++ university.sql
@@ -12,3 +12,24 @@ where `cfu` > 10;
 SELECT *
 FROM `students`
 WHERE YEAR(`date_of_birth`) <= 1991;
+
+--# 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
+
+--# 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT * 
+FROM `exams` 
+WHERE `date` = '2020/06/20' AND HOUR(`hour`) >= 14;
+
+--# 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT *
+FROM `degrees`
+WHERE `level` = 'Magistrale';
+
+--# 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT * 
+FROM `departments`;
+
+--# 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT *
+FROM `teachers`
+WHERE `phone` IS NULL
